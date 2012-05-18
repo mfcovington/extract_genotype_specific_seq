99d0aba7640268c8b8845a9557a8009faef77e28
Mike Covington <mfcovington@gmail.com>
2012-05-17 18:46:04 -0700

    get individual gene IDs or a list and search for each

--- extract_seq_by_geno.pl
+++ extract_seq_by_geno.pl
@@ -3,52 +3,55 @@
 # Mike Covington
 # created: 2012-05-17
 #
-# Description: 
+# Description:
 #
 use Modern::Perl;
+use autodie;
 use Bio::DB::Fasta;
 use Bio::SeqIO;
 use Getopt::Long;
 
-my ($fa_file, $match_id);
-
-my $options = GetOptions (
-    "fasta=s" => \$fa_file,
-    "id=s"    => \$match_id,
+#options
+my ( $fasta_in, $gene_id, $list );
+my $options = GetOptions(
+    "fasta_in=s" => \$fasta_in,
+    "gene_id=s"  => \$gene_id,
+    "list"       => \$list,
 );
 
-my $out = Bio::SeqIO->new(
-    -file   => ">outputfilename" ,
+#gather gene ids to search for
+my @gene_list;
+if ($list) {
+    open my $gene_list_fh, '<', $gene_id;
+    while ( my $line = <$gene_list_fh> ) {
+        chomp $line;
+        push @gene_list, $line;
+    }
+}
+else { push @gene_list, $gene_id; }
+
+#designate input/output files + formats
+my $seq_db = Bio::DB::Fasta->new($fasta_in);
+my $out    = Bio::SeqIO->new(
+    -file   => ">$gene_id.fasta",
     -format => 'Fasta'
 );
 
-my $seq_db = Bio::DB::Fasta->new($fa_file);
+for my $match_id (@gene_list) {
 
-#get all gene IDs that match query
-my @ids_all = $seq_db->ids;
-my @matches_full;
-for my $id (@ids_all) {
-    push @matches_full, $1 if $id  =~ m/(.*$match_id.*)/i; #what if multiple??
+    #get all gene IDs that match query
+    my @ids_all = $seq_db->ids;
+    my @matches_full;
+    for my $id (@ids_all) {
+        push @matches_full, $1 if $id =~ m{ ( \A .* $match_id .* \Z ) }ix;
+    }
+
+    #write output fasta file for all queries
+    for my $seq_id ( sort @matches_full ) {
+        my $seqobj = Bio::Seq->new(
+            -display_id => $seq_id,
+            -seq        => $seq_db->seq($seq_id),
+        );
+        $out->write_seq($seqobj);
+    }
 }
-
-#write output fasta file for all queries
-for my $seq_id (sort @matches_full) {
-    my $seqobj = Bio::Seq->new(
-        -display_id => $seq_id,
-        -seq        => $seq_db->seq($seq_id)
-    );
-    $out->write_seq($seqobj);
-}
-
-
-
-# my $db = Bio::DB::Fasta->new('ITAG2.3_cds_SHORTNAMES.fasta');
-# my @ids_all = $db->ids;
-# my $match_id = "Solyc00g016590.1.1";# "Solyc00g005000.2.1";
-
-# # my $seq = $db->seq('lyrata', 1 => 108);
-# my $seq = $db->seq($match_id);
-# say "seq:$seq." if defined $seq;
-# for my $id (@ids_all) {
-#     say $1 if $id  =~ m/(.*$match_id.*)/i;
-# }

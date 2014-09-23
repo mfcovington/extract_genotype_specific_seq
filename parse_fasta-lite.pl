4eb52167f226976f660ab71aeec832d3acfbea56
Mike Covington <mfcovington@gmail.com>
2014-09-23 14:47:54 -0700

    Add optional custom delimiter for seq ID

--- parse_fasta-lite.pl
+++ parse_fasta-lite.pl
@@ -8,10 +8,12 @@
 use strict;
 use warnings;
 use autodie;
-use feature 'say';
+use v5.10;
 
 my $gene_list = $ARGV[0];
 my $fasta     = $ARGV[1];
+my $delimiter = $ARGV[2] // '';
+$delimiter = quotemeta($delimiter);
 
 open my $gene_list_fh, "<", $gene_list;
 my %genes = map { chomp; $_ => 1 } <$gene_list_fh>;
@@ -21,7 +23,7 @@ open my $fa_fh,     "<", $fasta;
 open my $fa_out_fh, ">", "$gene_list.$fasta";
 my $hit = 0;
 while (<$fa_fh>) {
-    if (/^>(\S+)/) {
+    if (/^>([^\s$delimiter]+)/) {
         $hit = 0;
         $hit = 1 if exists $genes{$1};
     }
